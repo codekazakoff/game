4c3efa9e4099e8d0ededa4e1851a0e523e419e39
shohchakiller@gmail.com <shohchakiller@gmail.com>
2021-06-03 23:34:19 +0500

    tayyor game

---
+++ main.js
@@ -0,0 +1,25 @@
+const dino =  document.getElementById('dino');
+const cactus = document.getElementById('cactus');
+console.log(dino);
+
+document.addEventListener('keydown', (e) => {
+    jump();
+})
+
+const jump = () => {
+    if(dino.classList != 'jump'){
+        dino.classList.add('jump');
+    }
+    setTimeout( ()=>{
+        dino.classList.remove('jump');
+    },300)
+}
+
+let isAlive = setInterval( function(){
+        let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
+        let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+        if(cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
+            alert('Game Over !!!')
+        }
+},10)
