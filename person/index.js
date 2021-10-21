a531308da042273b3caee15e66b6cef129438201
shohchakiller@gmail.com <shohchakiller@gmail.com>
2021-10-22 01:41:43 +0500

    dqwd

--- person/index.js
+++ person/index.js
@@ -1,37 +1,36 @@
-window.addEventListener('load', ()=>{
-    let form = document.querySelector('form');
+window.addEventListener("load", () => {
+  let form = document.querySelector("form");
 
-    form.addEventListener('submit',(e)=>{
-        e.preventDefault();
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-        let formData = {
-            email: document.getElementById('email').value,
-            bissNumber : document.getElementById('number').value,
-            mobileNumber : document.getElementById('mobile').value
-        }
-        
-        var arr = JSON.parse(localStorage.getItem('formData')) || [];
+    let formData = {
+      email: document.getElementById("email").value,
+      bissNumber: document.getElementById("number").value,
+      mobileNumber: document.getElementById("mobile").value,
+    };
 
-        // arr.push(formData);
+    var arr = JSON.parse(localStorage.getItem("formData")) || [];
 
-        localStorage.setItem('formData', JSON.stringify([...arr, formData]));
-        
+    // arr.push(formData);
 
-        disptData();
-    })
+    localStorage.setItem("formData", JSON.stringify([...arr, formData]));
 
-    $('.file-btn').change( function(e) {
-        $("img").fadeIn("fast").attr('src',URL.createObjectURL(e.target.files[0]));        
-    });
+    disptData();
+  });
 
-    const disptData =() =>{
-        let data = JSON.parse(localStorage.getItem('formData'));
-        console.log(data)
+  $(".file-btn").change(function (e) {
+    $("img").fadeIn("fast").attr("src", URL.createObjectURL(e.target.files[0]));
+  });
 
-        const {email,bissNumber,mobileNumber} = data[data.length - 1];
+  const disptData = () => {
+    let data = JSON.parse(localStorage.getItem("formData"));
+    console.log(data);
 
-        document.querySelector('.email').append(`${email}`);
-        document.querySelector('.bissNumber').append(`${bissNumber}`);
-        document.querySelector('.mobileNumber').append(`${mobileNumber}`);
-    }
-})
+    const { email, bissNumber, mobileNumber } = data[data.length - 1];
+
+    document.querySelector(".email").append(`${email}`);
+    document.querySelector(".bissNumber").append(`${bissNumber}`);
+    document.querySelector(".mobileNumber").append(`${mobileNumber}`);
+  };
+});
